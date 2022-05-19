f0bdb766bc12d5b2b02e9d156c2a38dfced3f642
Mariia <mariia.neroznak@visma.com>
2022-05-19 11:12:38 +0300

    Remove warnings

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,12 +1,8 @@
 import React from 'react';
-// import faker from '@faker-js/faker';
-import CommentDetail from './CommentDetail';
 
 const ApprovalCard = (props) => {
-  // console.log(props.children);
   return (
     <div className='ui card'>
-      {/* <div className='content'>Are you sure?</div> */}
       <div className='content'>{props.children}</div>
       <div className='extra content'>
         <div className='ui two buttons'>
